772d4251afb138ad745e2afaff14068a8df00d46
DominikPeters <mail@dominik-peters.de>
2022-01-22 20:19:55 +0000

    Deploying to gh-pages from @ DominikPeters/tikz-action-test@1167be3b687de92eaf223c2f94c4a1f9abe70edf 🚀

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-lualatex -interaction=nonstopmode -halt-on-error pgfmanual.tex
-lwarpmk cleanall
-lwarpmk html
-./build-limages-with-margin.lua limages
-python3 postprocessing.py
-svgo -f processed/pgfmanual-images
--- latexbuild.sh
+++
@@ -1,4 +0,0 @@
-lualatex -interaction=nonstopmode -halt-on-error pgfmanual.tex
-lwarpmk cleanall
-lwarpmk html
-./build-limages-with-margin.lua limages
