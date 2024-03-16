2ae5e5b3f1ce43bc6dc0bf0c824114dabff90986
Dominik Peters <mail@dominik-peters.de>
2024-03-16 19:19:31 +0100

    pngquant, mathjax update

--- doc/generic/pgf/postprocessing.py
+++ doc/generic/pgf/postprocessing.py
@@ -387,8 +387,8 @@ def remove_mathjax_if_possible(filename, soup):
     with open(filename, "r") as file:
         content = file.read()
         if content.count("\(") == 61:
-            # mathjax isn't actually used
-            soup.find(class_="hidden").decompose()
+            # mathjax isn't actually used: delete div with "data-nosnippet"
+            soup.find("div", attrs={"data-nosnippet": True}).decompose()
             # remove element with id "MathJax-script"
             soup.find(id="MathJax-script").decompose()
             # go through all script tags and remove the ones that contain the word "emulation"
